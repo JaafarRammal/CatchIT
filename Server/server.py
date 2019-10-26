f2c2f35b61025a6ce6ba3d459a7fa24d3dda52a2
Prabhjot Grewal <prab99uk@gmail.com>
2019-10-26 21:39:23 +0100

    Created server communication with local database

--- Server/server.py
+++ Server/server.py
@@ -1,16 +1,42 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from json import dumps
-from flask_jsonpify import jsonify
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from config import Config
 
 app = Flask(__name__)
-api = Api(app)
+app.config.from_object(Config)
 
-class User(Resource):
-    def get(self):
-        return {'name' : 'testname'}
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-api.add_resource(User, '/users')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    points_collected = db.Column(db.Integer, default=0)
+
+    def __init__(self, name, points_collected):
+        self.name = name
+        self.points_collected = points_collected
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'points_collected')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    name = request.json['name']
+    points_collected = request.json['points_collected']
+
+    new_user = User(name, points_collected)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user)
+
 
 if __name__ == '__main__':
     app.run(port='5002')
