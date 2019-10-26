23b2ac02bbf522c7fa63ac39c44a41dabfc29136
ZiadAbass <el16za@leeds.ac.uk>
2019-10-26 20:37:50 +0100

    Added all Profile, Leaderboards and Store views to Storyboard

--- CatchIT/CatchIT/Profile.swift
+++ CatchIT/CatchIT/Profile.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+
+class ProfileView: UIViewController{
+    
+}
--- CatchIT/CatchIT/Store.swift
+++ CatchIT/CatchIT/Store.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2019 Jaafar Rammal. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StoreView: UIViewController{
+    
+}
+
+
