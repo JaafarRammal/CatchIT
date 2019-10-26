c681632bce9022429122d98647e05d7fdfbab18e
Jaafar Rammal <jaafarrammal@Sableur-Mac.local>
2019-10-26 18:26:48 +0100

    init views

---
+++ CatchIT/CatchIT/Leaderboard.swift
@@ -0,0 +1,9 @@
+//
+//  Leaderboard.swift
+//  CatchIT
+//
+//  Created by Jaafar Rammal on 10/26/19.
+//  Copyright © 2019 Jaafar Rammal. All rights reserved.
+//
+
+import Foundation
---
+++ CatchIT/CatchIT/Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  CatchIT
+//
+//  Created by Jaafar Rammal on 10/26/19.
+//  Copyright © 2019 Jaafar Rammal. All rights reserved.
+//
+
+import Foundation
